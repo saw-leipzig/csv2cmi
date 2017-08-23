acfc8be0feaa25b2008d1d587ee889cccee3fc6a
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2017-08-23 17:37:33 +0200

    adding setup and MANIFEST

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+package_name = 'csv2cmi'
+filename = package_name + '.py'
+
+
+def get_version():
+    import ast
+
+    with open(filename) as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
+
+
+def get_long_description():
+    try:
+        with open('README.md', 'r') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+setup(
+    name=package_name,
+    version=get_version(),
+    author='rettinghaus',
+    description='convert a table of letters into CMI format',
+    url='https://github.com/saw-leipzig/csv2cmi',
+    long_description=get_long_description(),
+    py_modules=[package_name],
+    license='License :: OSI Approved :: MIT License',
+)
